61cdd51c84ceebdb08db7878b64345c9aac5c912
Lucas Brown <lucasbrown1871@gmail.com>
2018-01-04 23:37:49 -0600

    This should have been part of last - forgot to save

--- QuennyPit.java
+++ QuennyPit.java
@@ -6,9 +6,7 @@ public class QuennyPit {
     public static void main( String[] args ) {
 
         PomTimer pom = new PomTimer();
-        PomTimer minute = new PomTimer( 60 );
 
         pom.start();
-        minute.start();
     }
 }
