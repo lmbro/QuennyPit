3396ea5363765ad2e1fa5eab9f878fe173030e9c
Lucas Brown <lucasbrown1871@gmail.com>
2018-02-01 21:41:28 -0600

    Updated resizing, added super class

--- python_prototype/fiftytwoseventeen.py
+++ python_prototype/fiftytwoseventeen.py
@@ -29,8 +29,8 @@ class FiftyTwoSeventeen( Tk ):
         self.master_frame.pack( expand=TRUE, fill=BOTH )
         
         self.clock.grid( row=0, column=0, columnspan=2, sticky=N+S+E+W )
-        self.btn_work.grid( row=1, column=0, sticky=E+W )
-        self.btn_rest.grid( row=1, column=1, sticky=E+W )
+        self.btn_work.grid( row=1, column=0, sticky=N+S+E+W )
+        self.btn_rest.grid( row=1, column=1, sticky=N+S+E+W )
 
         self.master_frame.rowconfigure( 0, weight=1 )
         self.master_frame.rowconfigure( 1, weight=1 )
@@ -52,33 +52,50 @@ class FiftyTwoSeventeen( Tk ):
         self.clock.start()
         return
 
-
-class Ticker( Frame ):
+class TickTock( Frame ):
 
     def __init__( self, parent, time=0 ):
         Frame.__init__( self, parent )
 
-        # Core operation
-        self.ticker = threading.Timer( 1, self._tick )
         self.time = time
         self.run = True
 
-        # GUI Elements
         self.time_str = StringVar()
-        self.font = Font( family="Source Code Pro", size=20 )
         self._format_time()
+        self.font = Font( family="Source Code Pro" )
+        
         Label( self, textvariable=self.time_str, font=self.font ).pack()
 
         self.bind( '<Configure>', self._resize )
+
+    def get_time( self ):
+
+        return self.time
+
+    def _format_time( self ):
+        self.time_str.set( "{:02d}:{:02d}".format(*divmod( self.time, 60)) )
+        return
+
+    def _resize( self, event ):
+        height = ( self.winfo_width() ) // -4
+        print( height )
+        self.font.configure( size=height )
+        return
+
+
+class Ticker( TickTock ):
+
+    def __init__( self, parent, time=0 ):
+        TickTock.__init__( self, parent, time )
+
+        # Core operation
+        self.ticker = threading.Timer( 1, self._tick )
 
     def start( self ):
         self._tick()
 
     def stop( self ):
         self.run = False
-
-    def get_time( self ):
-        return self.time
 
     def _tick( self ):
         self._format_time()
@@ -87,29 +104,14 @@ class Ticker( Frame ):
             self.ticker = threading.Timer( 1, self._tick )
             self.ticker.start()
 
-    def _format_time( self ):
-        self.time_str.set( "{:02d}:{:02d}".format(*divmod( self.time, 60)) )
 
-    def _resize( self, event ):
-        height = ( self.winfo_width() ) // -4
-        print( height )
-        self.font.configure( size=height )
-
-
-class Tocker( Frame ):
+class Tocker( TickTock ):
 
     def __init__( self, parent, time ):
-        Frame.__init__( self, parent )
+        TickTock.__init__( self, parent, time )
 
         # Core operation
         self.tocker = threading.Timer( 1, self._tock )
-        self.time = time
-        self.run = True
-
-        # GUI Elements
-        self.time_str = StringVar()
-        self._format_time()
-        Label( self, textvariable=self.time_str ).pack()
 
     def start( self ):
         self._tock()
@@ -117,31 +119,12 @@ class Tocker( Frame ):
     def stop( self ):
         self.run = False
     
-    def get_time( self ):
-        return self.time
-
     def _tock( self ):
         self._format_time()
         if( (self.time > 0) and (self.run) ):
             self.time -= 1
             self.tocker = threading.Timer( 1, self._tock )
             self.tocker.start()
-
-    def _format_time( self ):
-        self.time_str.set( "{:02d}:{:02d}".format(*divmod( self.time, 60)) )
-
-
-def tickTockTest():
-
-    stopwatch = Ticker()
-    stopwatch.start()
-
-    time.sleep( 10 )
-    
-    stopwatch.stop()
-
-    timer = Tocker( stopwatch.get_time() // 3 )
-    timer.start()
 
 
 if( __name__=='__main__' ):
